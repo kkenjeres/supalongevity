0c7f38d71cc7794c8188cd5af28ea9404cd3d2bb
kkenjeres <kkenjeres96@gmail.com>
2024-03-20 23:38:20 +0100

    import

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <header className="w-[90%] md:w-[80%] m-auto p-4 absolute top-2 right-0 left-0 z-10">
       <nav>
         <ul className="flex justify-between items-center text-white">
-          <li>supalongevity</li>
+          <li>supalongevityz</li>
           <li>FAQ</li>
           <li>
             <ButtonNav />
